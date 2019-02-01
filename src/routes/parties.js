e75d603ed1894171fb2856a8f3048c2fc3950b18
Okwara Emmanuel <okwarae.n@gmail.com>
2019-02-01 04:52:06 +0100

    feature(parties route): Connect to the parties controller
    [Delivers #163586720]

--- src/routes/parties.js
+++ src/routes/parties.js
@@ -6,4 +6,7 @@ const routes = Router();
 // Get a list of all political parties
 routes.get('/', PartiesController.getAllParties);
 
+// Get a specific political party
+routes.get('/:id', PartiesController.getSingleParty);
+
 export default routes;
