1c1cf16da08a090f0b33a5735d14c061f3ec2eb5
Okwara Emmanuel <okwarae.n@gmail.com>
2019-02-01 05:00:35 +0100

    feature(controller validation): Validate party details
    [Delivers #163586547]

--- src/controllers/validationFunctions.js
+++ src/controllers/validationFunctions.js
@@ -13,5 +13,19 @@ module.exports = {
       logoUrl: Joi.string().required()
     };
     return Joi.validate(office, schema);
+  },
+  validateParty: party => {
+    // TODO make it validate a url
+    const schema = {
+      name: Joi.string()
+        .min(10)
+        .required(),
+      hqAddress: Joi.string()
+        .min(10)
+        .required(),
+      logoUrl: Joi.string().required()
+    };
+
+    return Joi.validate(party, schema);
   }
 };
