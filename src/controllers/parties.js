dc99424392087f828f898a4d14da1fe1bbcd61b1
Okwara Emmanuel <okwarae.n@gmail.com>
2019-02-01 04:51:03 +0100

    feature(parties controller): Fetch a specific party
    Using the id check if the party exists and return its values
    [Deivers #163586720]

--- src/controllers/parties.js
+++ src/controllers/parties.js
@@ -7,6 +7,17 @@ class PartiesController {
   static getAllParties(req, res) {
     return res.json({ status: 200, data: politicalParties });
   }
+
+  static getSingleParty(req, res) {
+    // Make sure that the id exists in the database
+    const foundParty = politicalParties.find(party => party.id === parseInt(req.params.id, 10));
+
+    if (!foundParty)
+      return res.status(404).json({ status: 404, error: 'The political party does not exist' });
+
+    // The party exists return party details
+    return res.status(200).json({ status: 200, data: [foundParty] });
+  }
 }
 
 export default PartiesController;
