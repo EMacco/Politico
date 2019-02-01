a15b46d4146abca044c4a7a9623b6e7211717f96
Okwara Emmanuel <okwarae.n@gmail.com>
2019-02-01 04:20:35 +0100

    feature(offices route): Connect to controller and get result
    [Delivers #163587216]

--- src/routes/offices.js
+++ src/routes/offices.js
@@ -8,4 +8,7 @@ routes.get('/', OfficesController.getAllOffices);
 // Fetch a specific political office
 routes.get('/:id', OfficesController.getSingleOffice);
 
+// Create political office
+routes.post('/', OfficesController.createPoliticalOffice);
+
 export default routes;
