129f28d624d2953996abe5661c3b838782ba69ee
Okwara Emmanuel <okwarae.n@gmail.com>
2019-02-01 01:33:46 +0100

    chore(offices test): Test will return list of all offices
    Test will return all political parties that are in memory
    [Delivers #163585706]

---
+++ tests/offices.js
@@ -0,0 +1,23 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index';
+
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+
+describe('Political Offices', () => {
+  describe('GET /', () => {
+    // Test should return a list of all political offices
+    it('should get all political office', done => {
+      chai
+        .request(app)
+        .get('/api/v1/offices')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+});
