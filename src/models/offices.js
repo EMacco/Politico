1beeca915864133547eeb7a042e9a227e5a841e3
Okwara Emmanuel <okwarae.n@gmail.com>
2019-02-01 03:26:24 +0100

    feature(models): Create dummy data in models
    Create dummy object that will be used to store and retrieve offices, parties and users
    [Delivers #163587304]

---
+++ src/models/offices.js
@@ -0,0 +1,10 @@
+const politicalOffices = [
+  {
+    id: 1,
+    name: 'Office of the President',
+    type: 'Federal',
+    logoUrl: 'http://google.com/president-of-nigeria.png'
+  }
+];
+
+export default politicalOffices;
