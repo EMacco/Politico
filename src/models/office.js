60f6acd9c9dcbc4ef7d744cabcf85333bbab761a
Okwara Emmanuel <okwarae.n@gmail.com>
2019-02-07 08:46:59 +0100

    feature (Result): View election result
    Show the number of votes each candidate got at the end of the election
    [Delivers #163682686]

--- src/models/office.js
+++ src/models/office.js
@@ -1,6 +1,7 @@
 import pool from './db_connection';
+import OfficesModel from '../models/offices';
 
-class OfficeModel {
+class OfficeModel extends OfficesModel {
   static registerCandidate(officeId, partyId, candidateId, completionHandler) {
     const queryText = `INSERT INTO candidates(officeId, partyId, candidateId) VALUES('${officeId}', '${partyId}', '${candidateId}') RETURNING *`;
     pool
@@ -10,6 +11,19 @@ class OfficeModel {
       })
       .catch(err => {
         completionHandler({ success: false, data: err });
+      });
+    return null;
+  }
+
+  static collateResult(officeId, completionHandler) {
+    const queryText = `SELECT officeid, candidateid, COUNT(candidateid) FROM votes WHERE officeId='${officeId}' GROUP BY candidateid, officeid`;
+    pool
+      .query(queryText)
+      .then(res => {
+        completionHandler({ successs: true, dataa: res.rows });
+      })
+      .catch(err => {
+        completionHandler({ successs: false, dataa: err });
       });
     return null;
   }
--- src/routes/office.js
+++ src/routes/office.js
@@ -7,4 +7,7 @@ const routes = Router();
 // Register new election candidate
 routes.post('/:candidateId/register', Authorization.isUser, OfficeController.registerCandidate);
 
+// Fetch result for specific office
+routes.post('/:officeId/result', Authorization.isUser, OfficeController.collateResult);
+
 export default routes;
